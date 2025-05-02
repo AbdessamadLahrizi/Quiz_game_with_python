17ba080ef4df2a502f91146b6166009de3953c7f
abdessamad_tikota <abdsamadtiko@gmail.com>
2025-05-02 06:41:00 +0100

    I've added some conditions and a for loop to excute the all ansers in the library

---
+++ pycode.py
@@ -0,0 +1,45 @@
+# A dictionary that stores auestions and answers
+
+quiz = {
+    "question1": {
+        "question": "What is the capital of France ?",
+        "answer": "Paris"
+    },
+    "question2": {
+        "question": "What is the capital of Germany ?",
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question": "What is the capital of Spain ?",
+        "answer": "Madrid"
+    },
+    "question4": {
+        "question": "What is the capital of America ?",
+        "answer": "New York"
+    },
+    "question5": {
+        "question": "What is the capital of Egypt ?",
+        "answer": "Qaahira"
+    },
+    "question6": {
+        "question": "What is the capital of Morocco ?",
+        "answer": "Rabat"
+    },
+    "question7": {
+        "question": "What is the capital of Italy ?",
+        "answer": "Rome"
+    }
+}
+
+
+score = 0
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("answer? ")
+    if answer.lower() == value['answer'].lower():
+        score = score + 1
+        print("The answer is correct ! You've gained "+ str(score)+ " point " )
+    else:
+        print("You're answer is wrong, you're score: " + str(score))
+        print("The answer is : "+ value['answer'])
+    
